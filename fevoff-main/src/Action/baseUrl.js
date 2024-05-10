289f95b88dff5a76f5e7b815018d0f563161f244
Mayur Agrawal <mayur.agrawal@sap.com>
2024-05-10 15:48:40 +0530

    config

--- fevoff-main/src/Action/baseUrl.js
+++ fevoff-main/src/Action/baseUrl.js
@@ -1,4 +1,4 @@
 // export const baseURL = "https://fevoff-backend.onrender.com/api";
 
-//  export const baseURL = "http://localhost:3001/api";
-export const baseURL="http://44.217.232.3:3001/api"
+  export const baseURL = "http://localhost:3001/api";
+//export const baseURL="http://44.217.232.3:3001/api"
